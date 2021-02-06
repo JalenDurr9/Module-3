c83e0ca9359f3e5d781f6331f7b2fa1db06673f6
JalenDurr9 <77880306+JalenDurr9@users.noreply.github.com>
2021-02-06 09:21:06 -0600

    Add files via upload

---
+++ Module 3 Lab Activity (2).py
@@ -0,0 +1,31 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Name: Jalen Durr
+>>> # Date: 1-31-2021
+>>> print("Hello World")
+Hello World
+>>> print("Hello What Is Your Name?")
+Hello What Is Your Name?
+>>> Name1 = "Jalen Durr"
+>>> print("Welcome", Name1)
+Welcome Jalen Durr
+>>> Name2 = "Professor Maldonado"
+>>> print("Hello How Are You?", Name1)
+Hello How Are You? Jalen Durr
+>>> print("Hello How Are You?", Name2)
+Hello How Are You? Professor Maldonado
+>>> Area = ("3.141592653 * 7*7 = 153.9380m")
+>>> print("The Area of the circle is:", Area)
+The Area of the circle is: 3.141592653 * 7*7 = 153.9380m
+>>> MPG = ("325 Miles / 13 Gallons = 25")
+>>> print("If you drive 325 miles and use 13 gallons of gas the MPG is?:", MPG)
+If you drive 325 miles and use 13 gallons of gas the MPG is?: 325 Miles / 13 Gallons = 25
+>>> Celsius = ("105-32 * 5 / 9 = 40.5")
+>>> print("If you have a 105 degree fahrenheit temperature. The fahrenheit to celsius would be?", Celsius)
+If you have a 105 degree fahrenheit temperature. The fahrenheit to celsius would be? 105-32 * 5 / 9 = 40.5
+>>>  print("If I go on a vacation on wednesday (Day 3) and I leave for a 16 day trip. I will come back on Friday (Day 5).")
+ 
+SyntaxError: unexpected indent
+>>> print("If I go on a vacation on wednesday (Day 3) and I leave for a 16 day trip. I will come back on Friday (Day 5).")
+If I go on a vacation on wednesday (Day 3) and I leave for a 16 day trip. I will come back on Friday (Day 5).
+>>> 
